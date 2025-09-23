562cff41df75d176584fb9eed994b2d74556b547
Loïs Marteel <marteellois@gmail.com>
2025-09-23 11:31:16 +0200

    pb game over

--- assets/app.js
+++ assets/app.js
@@ -1,21 +1,23 @@
-const innocents = window.APP?.innocents || [];
-
 import Alpine from 'alpinejs';
-
 
 import './bootstrap.js';
 import './js/login';
 import './js/timer.js';
 import './js/popUpAgent.js';
-
 import './styles/app.css';
 
 window.Alpine = Alpine;
 Alpine.start();
 
+// Récupération sûre de la donnée injectée par Twig
+const innocents = (typeof window !== 'undefined' && Array.isArray(window.APP?.innocents))
+    ? window.APP.innocents
+    : [];
+
+// Maintenant on peut l'utiliser partout
 if (innocents.length > 1) {
     const agent = innocents[1];
     console.log(`/* ${agent.name} n'est pas dangereux. */`);
-} else{
-    console.log('nop')
+} else {
+    console.log('nop');
 }
--- src/Controller/GameOverController.php
+++ src/Controller/GameOverController.php
@@ -3,16 +3,27 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class GameOverController extends AbstractController
 {
     #[Route('/game-over', name: 'app_game_over')]
-    public function index(): Response
+    public function index(Request $request): Response
     {
+
+        $session = $request->getSession();
+
+        if ($session->has('endtime') || $session->get('endtime') > time()) {
+            $session->set('endtime', time() + 0);
+        }
+
+        $endtime = $session->get('endtime');
+
         return $this->render('game_over/index.html.twig', [
             'controller_name' => 'GameOverController',
+            'endtime' => $endtime,
         ]);
     }
 }
